4e1aa2329945dce320b7747c908a936fd1b6b1e1
nullity <70228821+nullity00@users.noreply.github.com>
2025-11-20 16:01:11 +0530

    no countdown

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,6 @@ import { usePathname } from "next/navigation";
 
 export default function Home() {
   const pathname = usePathname();
-  const [countdown, setCountdown] = useState(3);
 
   useEffect(() => {
     if (typeof window !== "undefined") {
@@ -21,42 +20,6 @@ export default function Home() {
   }, [pathname]);
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        backgroundColor: "white",
-        textAlign: "center",
-      }}
-    >
-      <h1
-        style={{
-          color: "black",
-          fontSize: "2.5rem",
-        }}
-      >
-        yAcademy
-      </h1>
-      <p style={{ fontSize: "1.5rem", margin: "10px 0", color: "grey" }}>
-        is now
-      </p>
-      <img
-        src="/electisec-logo.png"
-        alt="Electisec Logo"
-        width={250}
-        height={250}
-      />
-      <p
-        style={{
-          color: "grey",
-          marginTop: "5rem",
-        }}
-      >
-        Redirecting in {countdown}...
-      </p>
-    </div>
+    <></>
   );
 }
